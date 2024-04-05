acc2d8c48d5a4f96b5175fc408fa30a0a9a68207
Animesh-456 <anim29006@gmail.com>
2024-04-05 21:33:02 +0530

    server error resolved3

--- index.js
+++ index.js
@@ -2,11 +2,7 @@ import express from "express";
 import dotenv from "dotenv"
 import Connection from "./database/db.js";
 import bodyParser from "body-parser";
-import Routes from "./server/route.js"
-
 import newRoute from "./server/newRoute.js"
-
-import TaskRoutes from "./server/taskRoutes.js"
 import newTaskroutes from './server/newTaskRoutes.js'
 import cors from 'cors';
 
